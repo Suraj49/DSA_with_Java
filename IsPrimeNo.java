05a2236fea25885f858f9bb87bb8a51b84d6ab71
Suraj49 <1507surajkumar@gmail.com>
2022-02-25 22:14:31 +0530

    comitting 2 file

---
+++ IsPrimeNo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IsPrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalnNo;
+        System.out.println("Enter the Total no of Test case:");
+        totalnNo = sc.nextInt();
+        for (int j = 0; j < totalnNo; j++) {
+            int num;
+            System.out.println("Enter the No that you want to check:");
+            int flag = 0;
+            num = sc.nextInt();
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    flag = flag + 1;
+                }
+            }
+            if (flag == 2) {
+                System.out.println(num + " Is Prime Number");
+            } else {
+                System.out.println(num + " Is Not Prime Number");
+            }
+        }
+    }
+}
