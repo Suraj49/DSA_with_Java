db020c2d6313741b88cf482beddd2b70970d8cfa
Suraj49 <1507surajkumar@gmail.com>
2022-02-28 18:30:31 +0530

    comitting 2 file

---
+++ RotateANumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RotateANumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int digit=0;
+        int temp=n;
+        while (temp!=0){
+            temp = temp / 10;
+            digit++;
+        }
+        int pow=(int)Math.pow(10,k);//10^k
+        int left=n / pow;
+        int right=n % pow;
+        int mul=(int)Math.pow(10,digit-k);
+        int newNo=(right*mul)+left;
+        System.out.println(newNo);
+    }
+}
