edcf6101ad0669b2ac8225b3fc31f4200e37a630
Suraj49 <1507surajkumar@gmail.com>
2022-03-07 22:28:35 +0530

    comitting 1 file

--- Pattern_16.java
+++ Pattern_16.java
@@ -4,30 +4,28 @@ public class Pattern_16 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
-        int fstar=1;
-        int lstar=1;
-        int space=n+1;
+        int star=1;
+        int space=(n*2)-3;
         for(int i=1;i<=n;i++) {
-            int temp=1;
             int num=1;
-            for (int fst=1;fst<=fstar;fst++){
+            for (int fst=1;fst<=star;fst++){
                 System.out.print(num+"\t");
-                temp=num;
                 num++;
             }
             for (int spc=1;spc<=space;spc++){
                 System.out.print("\t");
             }
-
-            for (int lst = 1; lst <= fstar; lst++) {
-                System.out.print(temp + "\t");
-                temp--;
-                }
+            if ( i == n){
+                star--;
+                num--;
+            }
+            for (int lst = 1; lst <= star; lst++) {
+                num--;
+                System.out.print(num + "\t");
+            }
             System.out.println();
             space=space-2;
-            fstar++;
-
+            star++;
         }
-
     }
 }
