9d6f4df49c27bdfcf9dd67d9139a7edb50bda426
Suraj49 <1507surajkumar@gmail.com>
2022-03-08 15:44:31 +0530

    comitting 1 file

---
+++ BasicArray.java
@@ -0,0 +1,12 @@
+public class BasicArray {
+    public static void main(String[] args) {
+        int []arr;
+        arr=new int[5];
+        arr[0]=0;
+        arr[2]=20;
+        arr[1]=10;
+        arr[3]=30;
+        arr[4]=40;
+        System.out.println(arr[3]);
+    }
+}
