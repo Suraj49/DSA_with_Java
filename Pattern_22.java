1f4a8a40f1d78410da939450ca5dfb568c9d587d
Suraj49 <1507surajkumar@gmail.com>
2022-03-08 10:33:02 +0530

    comitting 1 file

---
+++ Pattern_22.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pattern_22 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int space = 0;
+        int star = n;
+        for (int i = 1; i <= n; i++) {
+            for (int spc = 1; spc <= space; spc++) {
+                System.out.print("\t");
+            }
+            for (int str = 1; str <= star; str++) {
+                if ( i > n/2 && i < n  && str > 1 && str < star) {
+                    System.out.print("\t");
+                }
+                else {
+                    System.out.print("*\t");
+                }
+            }
+            if ( i <= n / 2) {
+                star -= 2;
+                space++;
+            }
+            else {
+                star += 2;
+                space--;
+            }
+            System.out.println();
+        }
+    }
+}
