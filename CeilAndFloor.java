3da87a1aea9b7df1739f93ebfd9429ea5d7b81f5
Suraj49 <1507surajkumar@gmail.com>
2022-03-16 19:17:50 +0530

    comitting 1 file

---
+++ CeilAndFloor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CeilAndFloor {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        int data = sc.nextInt ();
+        int ceil = Integer.MAX_VALUE;
+        int floor = Integer.MIN_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (data < arr[mid]) {
+                high = mid - 1;
+                ceil = arr[mid];
+            }
+            else if (data > arr[mid]) {
+                low = mid + 1;
+                floor = arr[mid];
+            } else {
+                ceil = floor = arr[mid];
+                break;
+            }
+
+        }
+        System.out.println (ceil);
+        System.out.println (floor);
+    }
+}
