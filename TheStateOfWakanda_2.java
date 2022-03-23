d1799571fd80222e2f18720a8e3ffc1848ee2bd2
Suraj49 <1507surajkumar@gmail.com>
2022-03-23 23:06:20 +0530

    comitting 1 file

--- TheStateOfWakanda_2.java
+++ TheStateOfWakanda_2.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 public class TheStateOfWakanda_2 {
    public static Scanner sc = new Scanner(System.in);
-    public static void input(int arr[][]){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                arr[i][j]=sc.nextInt();
-            }
-        }
-    }
-
     public static void print(int arr[][]){
         int n = arr.length;
         for(int gap = 0 ; gap < n; gap++){
@@ -23,8 +15,12 @@ public class TheStateOfWakanda_2 {
     public static void main(String[] args) {
         int n= sc.nextInt();
         int arr[][]=new int[n][n];
-        input(arr);
-        print(arr);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
 
+        print(arr);
     }
 }
